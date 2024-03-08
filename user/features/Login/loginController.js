4ae469af4e7b6a50e11c49f625ae85a1ecce149c
ysriv17 <138852360+ysriv17@users.noreply.github.com>
2024-03-09 03:21:40 +0530

    Update loginController.js

--- user/features/Login/loginController.js
+++ user/features/Login/loginController.js
@@ -9,9 +9,9 @@ const Login = async (req, res, next) => {
         let user = await signup.findOne({ "verifieduser.email": `${email}` })
         console.log(user)
         if (user === null) {
-         
+              console.log("user not found")
             res.json({ message:"user not found"});
-               console.log("user not found")
+          
             // throw new Error("user not valid");
         }
         else if (user.verifieduser.email == email && await bcrypt.compare(password, user.verifieduser.password)) {
@@ -22,8 +22,9 @@ const Login = async (req, res, next) => {
             
         }
         else {
+            console.log("im in else")
             res.status(100).json({ message: "wrong password" });
-              console.log("im in else")
+              
             // throw new Error("wrong password")
         }
     }
