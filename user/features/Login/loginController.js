3f81f994c159c20ce2229dcf6505432e7e404d59
ysriv17 <138852360+ysriv17@users.noreply.github.com>
2024-03-09 02:28:51 +0530

    Update loginController.js

--- user/features/Login/loginController.js
+++ user/features/Login/loginController.js
@@ -10,7 +10,7 @@ const Login = async (req, res, next) => {
         console.log(user)
         if (user === null) {
             console.log("user not found")
-            res.status(100).json("user not found");
+            res.status(100).json({ message:"user not found"});
             
             // throw new Error("user not valid");
         }
